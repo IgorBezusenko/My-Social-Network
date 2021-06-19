557ad010f07549a841d65ce3e5a4208bd5e0ae43
IgorBezusenko <bezusenko.98@mail.ru>
2021-06-19 23:20:45 +0300

    saved changes

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,4 +1,5 @@
-import * as axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { ProfileType } from "../types/types";
 
 const instance = axios.create({
   withCredentials: true,
@@ -9,33 +10,30 @@ const instance = axios.create({
 });
 
 export const usersAPI = {
-  getUsers(currentPage, pageSize) {
+  getUsers(currentPage = 1, pageSize = 10) {
     return instance
       .get(`users?page=${currentPage}&count=${pageSize}`)
       .then((response) => response.data);
   },
-
-  follow(userId) {
+  follow(userId: number) {
     return instance.post(`follow/${userId}`);
   },
-  unfollow(userId) {
+  unfollow(userId: number) {
     return instance.delete(`follow/${userId}`);
   },
 };
 
 export const profileAPI = {
-  getProfile(userId) {
+  getProfile(userId: number) {
     return instance.get(`profile/${userId}`);
   },
-  getStatus(userId) {
+  getStatus(userId: number) {
     return instance.get(`profile/status/${userId}`);
   },
-
-  updateStatus(status) {
+  updateStatus(status: string) {
     return instance.put(`profile/status/`, { status: status });
   },
-
-  savePhoto(filePhoto) {
+  savePhoto(filePhoto: any) {
     const formData = new FormData();
     formData.append("image", filePhoto);
     return instance.put(`/profile/photo/`, formData, {
@@ -44,7 +42,7 @@ export const profileAPI = {
       },
     });
   },
-  saveProfile(profile) {
+  saveProfile(profile: ProfileType) {
     return instance.put(`profile`, profile);
   },
 };
@@ -53,7 +51,12 @@ export const authAPI = {
   me() {
     return instance.get(`auth/me`);
   },
-  login(email, password, rememberMe = false, captcha = null) {
+  login(
+    email: string,
+    password: string,
+    rememberMe = false,
+    captcha: null | string = null
+  ) {
     return instance.post(`auth/login`, {
       email,
       password,
